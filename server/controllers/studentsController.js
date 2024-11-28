7e6aa440a4dcfb344982ac28ad15d0eabe296b2d
Ragapiriya <r2001piriya@gmail.com>
2024-11-28 17:44:04 +0530

    Last commit

--- server/controllers/studentsController.js
+++ server/controllers/studentsController.js
@@ -1,3 +1,4 @@
+const { connect } = require("mongoose");
 const mysql = require("mysql2");
 const con = mysql.createPool({
   connectionLimit: 10,
@@ -36,12 +37,13 @@ exports.save = (req, res) => {
       console.log("Connection success");
       const { name, age, city } = req.body;
       connection.query(
-        "insert into users (NAME,AGE,CITY) values (?,?,?)",[name,age,city],
+        "insert into users (NAME,AGE,CITY) values (?,?,?)",
+        [name, age, city],
         (err, rows) => {
           connection.release();
           if (!err) {
             console.log("Good, works fine!!");
-            res.render("adduser", { msg:"User details added successfully" });
+            res.render("adduser", { msg: "User details added successfully" });
           } else {
             console.log("Error while fetching data" + err);
           }
@@ -50,3 +52,96 @@ exports.save = (req, res) => {
     }
   });
 };
+
+exports.edituser = (req, res) => {
+  con.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connection success");
+      //getting ID from url
+      let id = req.params.id;
+      connection.query(
+        "select * from users where id = ?",
+        [id],
+        (err, rows) => {
+          connection.release();
+          if (!err) {
+            console.log("Good, works fine!!");
+            res.render("edituser", { rows });
+          } else {
+            console.log("Error while fetching data" + err);
+          }
+        }
+      );
+    }
+  });
+};
+
+exports.editsave = (req, res) => {
+  con.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connection success");
+      let id = req.params.id;
+      const { name, age, city } = req.body;
+      connection.query(
+        "update users set NAME=?, AGE =?, CITY=? where ID=?",
+        [name, age, city, id],
+        (err, rows) => {
+          connection.release();
+          if (!err) {
+            console.log("Good, works fine!!");
+            con.getConnection((err, connection) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Connection success");
+                //getting ID from url
+                let id = req.params.id;
+                connection.query(
+                  "select * from users where id = ?",
+                  [id],
+                  (err, rows) => {
+                    connection.release();
+                    if (!err) {
+                      console.log("Good, works fine!!");
+
+                      res.render("edituser", {
+                        rows,
+                        msg: "User details updated successfully",
+                      });
+                    } else {
+                      console.log("Error while fetching data" + err);
+                    }
+                  }
+                );
+              }
+            });
+          } else {
+            console.log("Error while fetching data" + err);
+          }
+        }
+      );
+    }
+  });
+};
+
+exports.delete = (req, res) =>{
+  con.getConnection((err,connection)=>{
+    if(err) throw err
+    let id = req.params.id;
+    connection.query("delete from users where id=?",[id],(err,rows)=>{
+      connection.release();
+      if(!err)
+      {
+        res.redirect("/");
+      }
+      else
+      {
+        console.log(err);
+      }
+    })
+  })
+}
--- server/routes/students.js
+++ server/routes/students.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const studentsController = require("../controllers/studentsController");
 
 router.get("/",studentsController.view);
+
+//add new student
 router.get("/adduser",studentsController.adduser);
 router.post("/adduser",studentsController.save);
+
+//edit user
+router.get("/edituser/:id",studentsController.edituser);
+router.post("/edituser/:id",studentsController.editsave);
+
+
+
+//delete user
+router.get("/deleteuser/:id",studentsController.delete);
+
 
 
 module.exports = router;
